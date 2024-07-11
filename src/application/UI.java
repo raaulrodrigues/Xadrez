598310e6284d26482a8ee85f002eb36394524038
raaulrodriguees <clovisadavildo@gmail.com>
2024-07-11 10:29:13 -0300

    Adicionado movimento das peças

--- src/application/UI.java
+++ src/application/UI.java
@@ -2,7 +2,11 @@ package application;
 
 import chess.ChessMatch;
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
     public static final String ANSI_RESET = "\u001B[0m";
@@ -24,6 +28,17 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static ChessPosition readChessPosition(Scanner sc){
+        try{
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        }catch(RuntimeException e){
+            throw new InputMismatchException("Erro!! Só utilize posições de a1-h8!!");
+        }
+
+    }
     public static void printBoard(ChessPiece[][] pieces){
         for(int i=0; i<pieces.length; i++){
             System.out.print((8 - i) + " ");
@@ -32,7 +47,7 @@ public class UI {
             }
             System.out.println();
         }
-        System.out.println("  A B C D E F G H");
+        System.out.println("  a b c d e f g h");
     }
 
     private static void printPiece(ChessPiece piece) {
@@ -49,4 +64,6 @@ public class UI {
         }
         System.out.print(" ");
     }
+
+
 }
