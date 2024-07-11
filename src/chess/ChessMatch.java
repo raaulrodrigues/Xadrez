c33d371b3a3e33ed816993d248a1e3be3cd1bbf5
raaulrodriguees <clovisadavildo@gmail.com>
2024-07-11 08:22:42 -0300

    Adicionado cor para os times

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,9 +25,18 @@ public class ChessMatch {
         board.placePiece(piece,new ChessPosition(column, row).toPosition());
     }
     private void initialSetup(){
-        placeNewPiece('b', 6, new Torre(board, Color.WHITE));
-        placeNewPiece('e', 8, new Rei(board, Color.BLACK));
-        placeNewPiece('e', 1, new Rei(board, Color.WHITE));
-        placeNewPiece('c', 3, new Torre(board, Color.WHITE));
+        placeNewPiece('c', 1, new Torre(board, Color.WHITE));
+        placeNewPiece('c', 2, new Torre(board, Color.WHITE));
+        placeNewPiece('d', 2, new Torre(board, Color.WHITE));
+        placeNewPiece('e', 2, new Torre(board, Color.WHITE));
+        placeNewPiece('e', 1, new Torre(board, Color.WHITE));
+        placeNewPiece('d', 1, new Rei(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Torre(board, Color.BLACK));
+        placeNewPiece('c', 8, new Torre(board, Color.BLACK));
+        placeNewPiece('d', 7, new Torre(board, Color.BLACK));
+        placeNewPiece('e', 7, new Torre(board, Color.BLACK));
+        placeNewPiece('e', 8, new Torre(board, Color.BLACK));
+        placeNewPiece('d', 8, new Rei(board, Color.BLACK));
     }
 }
